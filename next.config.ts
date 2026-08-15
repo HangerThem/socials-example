aba2acd35e69d5becacbc82455979289675d81ed
HangerThem <f.borisjuk@gmail.com>
2026-08-15 11:54:14 +0200

    Initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: "/media/:path*",
+        destination: "/uploads/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
